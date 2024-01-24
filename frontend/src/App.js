9890299b212b6fad4c756b376f6f2497095fc422
ddang-jung <hjk3376@gmail.com>
2024-01-24 17:51:29 +0900

    feat: router init

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import Router from './router/Router.js';
+import Component from './core/Component.js';
+import createPages from './test/index.js';
+
+export default class App extends Component {
+	template() {
+		return `
+		<header>
+		<a href="#/">Home</a>
+		<a href="#/counter">Counter</a>
+		<a href="#/fetch">Fetch</a>
+		</header>
+		<main></main>
+		`;
+	}
+
+	mounted() {
+		const $main = this.$target.querySelector('main');
+		const pages = createPages($main);
+
+		const router = new Router($main);
+		router.addRoute('#/', pages.home);
+		router.addRoute('#/counter', pages.counter);
+		router.addRoute('#/fetch', pages.fetch);
+		router.start();
+	}
+}
