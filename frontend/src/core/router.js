b02e688e9e3764c03d465eddfc8e849b1a8cfb6a
Clearsu <clearwater1219@gmail.com>
2024-01-26 04:25:36 +0900

    refactor: implement router using closure

---
+++ frontend/src/core/router.js
@@ -0,0 +1,42 @@
+let routerInstance;
+
+const router = (routes) => {
+  if (routerInstance) {
+    return routerInstance;
+  }
+
+  const handleRouteChange = () => {
+    const path = window.location.pathname;
+    const createComponent = routes[path];
+    if (createComponent) {
+      const component = createComponent(document.getElementById("app"));
+      component.render();
+    }
+  };
+
+  const handleLinkClick = (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      const clickedURL = e.target.href;
+      if (clickedURL !== window.location.href) {
+        navigate(clickedURL);
+      }
+    }
+  };
+
+  window.addEventListener("popstate", handleRouteChange);
+  window.addEventListener("click", handleLinkClick);
+
+  const navigate = (path) => {
+    window.history.pushState({}, "", path);
+    handleRouteChange();
+  };
+
+  handleRouteChange();
+
+  routerInstance = { navigate };
+
+  return routerInstance;
+}; // IIFE를 사용하면 더 효율적
+
+export default router;
