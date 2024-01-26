d348e8b566028005a17afbeb4ae64c49a80971e8
Clearsu <clearwater1219@gmail.com>
2024-01-26 20:50:34 +0900

    feat: test page for testing components

---
+++ frontend/src/pages/JincparkTestPage.js
@@ -0,0 +1,20 @@
+import Input from "../components/Input.js";
+import Component from "../core/Component.js";
+
+export default class JincparkTestPage extends Component {
+  template() {
+    return `<h1>Jincpark's Component Test Page</h1><div id="test">`;
+  }
+
+  mounted() {
+    const component = new Input(this.$target.querySelector("#test"), {
+      type: "text",
+      id: "input test",
+      name: "name is for identifying this form when POST",
+      className: "",
+      placeholder: "hello world",
+      autocomplete: true,
+    });
+    component.render();
+  }
+}
