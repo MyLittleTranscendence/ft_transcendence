2e5825765fc005442cfca31e0cbeb290f59c5fcf
middlefitting <wjdtmdcjf2@gmail.com>
2024-01-26 20:46:29 +0900

    refactor(custom_auth): constant 캡슐화

--- backend/custom_auth/oauth_service.py
+++ backend/custom_auth/oauth_service.py
@@ -1,5 +1,5 @@
 import requests
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import APIException
 
 from custom_auth.oauth_42_constant import Oauth42Constant
 
@@ -10,13 +10,13 @@ class Oauth42Service:
         token_request = Oauth42Constant.token_uri + f"&code={code}"
         token_response = requests.post(token_request)
         if not token_response.ok:
-            raise ValidationError("내부 서버 에러, 인증 실패")
+            raise APIException("내부 서버 에러, 인증 실패")
         return token_response.json().get("access_token")
 
     def get_oauth_user_info(self, access_token):
-        user_info_response = requests.get(self.user_info_uri, params={'access_token': access_token})
+        user_info_response = requests.get(Oauth42Constant.user_info_uri, params={'access_token': access_token})
         if not user_info_response.ok:
-            raise ValidationError("내부 서버 에러, 프로필 조회 실패")
+            raise APIException("내부 서버 에러, 프로필 조회 실패")
         return user_info_response.json()
 
 
