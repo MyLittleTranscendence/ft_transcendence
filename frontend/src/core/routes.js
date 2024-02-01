79e037b06679bc4f53bcf452ca1805bea89bdea0
Clearsu <clearwater1219@gmail.com>
2024-02-01 20:26:11 +0900

    feat(UI): SignInPage

--- frontend/src/core/routes.js
+++ frontend/src/core/routes.js
@@ -4,6 +4,7 @@ import MyPage from "../pages/MyPage.js";
 import ProfilePage from "../pages/ProfilePage.js";
 import PvPGamePage from "../pages/PvPGamePage.js";
 import SignUpPage from "../pages/SignUpPage.js";
+import SignInPage from "../pages/SignInPage.js";
 import TournamentGamePage from "../pages/TournamentGamePage.js";
 import JincparkTestPage from "../pages/JincparkTestPage.js";
 import HyeonjunTestPage from "../pages/HyeonjunTestPage.js";
@@ -11,6 +12,7 @@ import HyeonjunTestPage from "../pages/HyeonjunTestPage.js";
 const routes = {
   "/": (element) => new LobbyPage(element),
   "/landing": (element) => new LandingPage(element),
+  "/sign-in": (element) => new SignInPage(element),
   "/sign-up": (element) => new SignUpPage(element),
   "/pvp": (element) => new PvPGamePage(element),
   "/tournament": (element) => new TournamentGamePage(element),
