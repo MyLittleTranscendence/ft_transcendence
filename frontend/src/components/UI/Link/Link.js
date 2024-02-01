0f4f057eb31cc4fc3017fe727e1bd6a72fb10460
Clearsu <clearwater1219@gmail.com>
2024-02-01 20:25:45 +0900

    feat(Link): button style link

---
+++ frontend/src/components/UI/Link/Link.js
@@ -0,0 +1,24 @@
+import Component from "../../../core/Component.js";
+import appendCSSLink from "../../../utils/appendCSSLink.js";
+
+appendCSSLink("src/components/UI/Link/Link.css");
+
+export default class Link extends Component {
+  template() {
+    const { href, small, content, className, attributes } = this.props;
+
+    return `
+      <a
+        href="${href}"
+        class="
+          btn rounded-pill link
+          ${className ? `${className}` : ""}
+          ${small ? "btn-sm" : "btn-lg"}
+        "
+        ${attributes || ""}
+      >
+        ${content}
+      </a>
+    `;
+  }
+}
