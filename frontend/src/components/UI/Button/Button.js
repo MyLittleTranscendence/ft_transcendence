5a5c603bd5da766dcad25334b8fd79903b995dae
ddang-jung <hjk3376@gmail.com>
2024-02-02 20:21:55 +0900

    fix: add missing double quote

--- frontend/src/components/UI/Button/Button.js
+++ frontend/src/components/UI/Button/Button.js
@@ -22,7 +22,7 @@ export default class Button extends Component {
         id="${id}"
         type="${type || "button"}"
         ${name ? `name="${name}"` : ""}
-        class="btn rounded-pill button-custom
+        class="btn rounded-pill button-custom"
         ${className ? `${className}` : ""}
         ${small ? "btn-sm" : "btn-lg"}"
         ${disabled || loading ? "disabled" : ""}
