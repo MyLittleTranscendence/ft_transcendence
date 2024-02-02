a1f7fe1571691b4e44a61178a29ff583152268d4
Clearsu <clearwater1219@gmail.com>
2024-02-02 20:58:44 +0900

    feat(UI): TwoFAForm

--- frontend/src/components/SignIn/TwoFAForm.js
+++ frontend/src/components/SignIn/TwoFAForm.js
@@ -5,51 +5,102 @@ import Button from "../UI/Button/Button.js";
 export default class TwoFAForm extends Component {
   template() {
     return `
-      <div 
+      <form
+        id="two-fa-form"
         class="
-        input-group
         d-flex
+        flex-column
         align-items-center
-        justify-content-center
-        my-3
-      ">
-        <label
+        "
+      >
+        <div
+          id="two-fa-group-container"
           class="
-          fw-bold fs-5 info-input-label-text
-          position-absolute
+            input-group
+            d-flex
+            align-items-center
+            justify-content-center
+            position-relative
           "
         >
-          Verification Code
-        </label>
-        <div id="two-fa-input-holder" class="mx-3"></div>
-        <div id="send-code-button-holder"></div>
-        <span class="
-          info-input-warning-text
-          position-absolute
-        ">
-          Wrong verification code
-        </span>
-      </div>
+          <label
+            for="two-fa-code-input"
+            class="fw-bold"
+            style="color: #b2b2b2"
+          >
+            Verification Code
+          </label>
+          <span
+            id="two-fa-timer"
+            class="text-white fw-semibold position-absolute"
+            style="transform: translate(200%, -120%);"
+          >
+            03:00
+          </span>
+        </div>
+        <span
+          id="two-fa-warning"
+          class="mt-1 fw-bold"
+          style="color: #FF9D9D;"
+        >Wrong verification code</span>
+      </form>
 		`;
   }
 
   mounted() {
-    const codeInput = new Input(
-      this.$target.querySelector("#two-fa-input-holder"),
+    const $container = this.$target.querySelector("#two-fa-group-container");
+    const codeInput = new Input($container, {
+      type: "text",
+      id: "two-fa-code-input",
+      name: "two-fa-code",
+      placeholder: "Your code",
+      autocomplete: false,
+      required: true,
+      className: "mx-2",
+    });
+    const sendCodeButton = new Button($container, {
+      small: true,
+      name: "send-code-btn",
+      content: "Send again",
+    });
+    const confirmButton = new Button(
+      this.$target.querySelector("#two-fa-form"),
       {
-        type: "text",
-        name: "two-fa-code",
-        placeholder: "Your code",
-        autocomplete: false,
-        required: true,
+        name: "two-fa-confirm",
+        content: "Confirm",
+        type: "submit",
+        className: "mt-5",
+        attributes: `style="min-width: 10rem;"`,
       }
-    );
-    const sendCodeButton = new Button(
-      this.$target.querySelector("#send-code-button-holder"),
-      { small: true, name: "send-code-btn", content: "Send again" }
     );
 
     codeInput.render();
     sendCodeButton.render();
+    confirmButton.render();
+
+    this.startTimer(179);
+  }
+
+  startTimer(duration) {
+    let timer = duration;
+    let minutes;
+    let seconds;
+    const interval = setInterval(() => {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? `0${minutes}` : minutes;
+      seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+      const $timerElement = this.$target.querySelector("#two-fa-timer");
+      $timerElement.textContent = `${minutes}:${seconds}`;
+
+      timer -= 1;
+      if (timer < 0) {
+        clearInterval(interval);
+        $timerElement.textContent = "00:00";
+        // Confirm 버튼 비활성화 로직
+      }
+    }, 1000);
   }
 }
