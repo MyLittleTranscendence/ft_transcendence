3d89b5694386a8784061b7ee8f22a1e32efe6265
ddang_jung <100337589+ddang-jung@users.noreply.github.com>
2024-02-05 16:39:58 +0900

    feat: 프로필 UI 컴포넌트 구현 (#104)
    * feat: init profile component
    
    * style: apply prettier
    
    * chore: move directory

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,5 +1,42 @@
 import Component from "../core/Component.js";
+import PageContainerWithLogo from "../components/UI/Container/PageContainerWithLogo.js";
+import Profile from "../components/Profile/Profile.js";
+import fetchAPI from "../utils/fetch/fetchAPI.js";
 
 export default class ProfilePage extends Component {
-  template = () => `<h1>User Profile Page</h1>`;
+  template() {
+    return `
+      <div
+        id="profile-container"
+        class="d-flex flex-column align-items-center"
+      >
+        <div
+          id="profile-content"
+        >
+        </div>
+      </div>
+    `;
+  }
+
+  mounted() {
+    const $profileContainer = this.$target.querySelector("#profile-container");
+
+    const pageContainer = new PageContainerWithLogo(
+      this.$target,
+      $profileContainer
+    );
+    const profileContent = new Profile($profileContainer, {
+      username: "hyeonjun",
+      imageSize: "image-mid",
+      imageSrc: "asset/default.png",
+      alt: "my profile",
+      wins: 42,
+      losses: 42,
+    });
+    // const matchHistoryContent = new MatchHistory($profileContainer, );
+
+    pageContainer.render();
+    profileContent.render();
+    // matchHistoryContent.render();
+  }
 }
