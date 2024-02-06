2d716b00f35be96f0cf4b865430c71e14abbd90f
Clearsu <clearwater1219@gmail.com>
2024-02-06 22:58:48 +0900

    feat(2FA): two factor authentication

--- frontend/src/pages/JincparkTestPage.js
+++ frontend/src/pages/JincparkTestPage.js
@@ -1,33 +1,40 @@
 import Component from "../core/Component.js";
-// import LoginForm from "../components/Login/LoginForm.js";
-// import SignUpForm from "../components/SignUp/SignUpForm.js";
-// import ChatInput from "../components/Lobby/ChatInput.js";
 import TwoFAForm from "../components/SignIn/TwoFAForm.js";
+import Button from "../components/UI/Button/Button.js";
+import fetchDisableTwoFA from "../api/auth/fetchDisableTwoFA.js";
 
 export default class JincparkTestPage extends Component {
+  setup() {
+    this.state = { mfa: sessionStorage.getItem("mfa_require") };
+  }
+
   template() {
     return `
       <div class="d-flex flex-column">
         <h1>Test Page</h1>
         <div id="test" class="mt-5"></div>
+        비활성화나 코드인증 하고 새로고침 해야 됨
       </div>`;
   }
 
   mounted() {
     const $div = this.$target.querySelector("#test");
 
-    // const loginForm = new LoginForm($div);
-    // const signupForm = new SignUpForm($div);
-    // const messageInput = new MessageForm($div, {
-    //   id: "message-input-test",
-    //   name: "message-input",
-    // });
-
-    // loginForm.render();
-    // signupForm.render();
-    // messageInput.render();
-
-    const verifyForm = new TwoFAForm($div);
-    verifyForm.render();
+    if (this.state.mfa === "false") {
+      const twoFAForm = new TwoFAForm($div, { type: "enable" });
+      twoFAForm.render();
+    } else {
+      const twoFADisableButton = new Button($div, {
+        id: "two-fa-disable-btn",
+        content: "Disable 2FA",
+        small: true,
+      });
+      twoFADisableButton.render();
+      twoFADisableButton.addEvent(
+        "click",
+        "#two-fa-disable-btn",
+        fetchDisableTwoFA
+      );
+    }
   }
 }
