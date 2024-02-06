2a743ab02f1c4a22261f7ac44d8f77ee54d055a9
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-06 17:34:14 +0900

    feat(chat): 인증 미들웨어 구현

--- backend/chat/middleware.py
+++ backend/chat/middleware.py
@@ -9,7 +9,6 @@ from rest_framework_simplejwt.tokens import AccessToken
 @database_sync_to_async
 def get_user_from_jwt(token):
     try:
-        # JWT 토큰 검증
         decoded_data = AccessToken(token)
         user_id = decoded_data['user_id']
         return get_user_model().objects.get(id=user_id)
@@ -17,33 +16,29 @@ def get_user_from_jwt(token):
         return AnonymousUser()
 
 
+def get_cookie_header(scope):
+    for header in scope['headers']:
+        if header[0].decode('utf-8') == 'cookie':
+            return header[1].decode('utf-8')
+    return ''
+
+
 class JWTAuthMiddleware:
-    """
-    Custom JWT authentication middleware for Django Channels
-    """
     def __init__(self, inner):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        # 쿠키에서 JWT 토큰 추출
-        cookies = scope.get('cookies', {})
-        access_token = cookies.get('access_token')
 
-        print(f"Connected user: {access_token}")
-        print(f"Connected user: {access_token}")
-        print(f"Connected user: {access_token}")
-        print(f"Connected user: {access_token}")
-        print(f"Connected user: {access_token}")
-
-
+        cookies = get_cookie_header(scope).split(";")
+        cookies = list(map(lambda cookie: cookie.strip(), cookies))
+        access_tokens = [cookie.split('=', 1)[1] for cookie in cookies if cookie.startswith('access_token=')]
+        access_token = access_tokens[0] if access_tokens else None
         if access_token:
             scope['user'] = await get_user_from_jwt(access_token)
         else:
             scope['user'] = AnonymousUser()
-
         return await self.inner(scope, receive, send)
 
 
-# 미들웨어 스택에 사용자 정의 미들웨어 포함
 def JWTAuthMiddlewareStack(inner):
     return JWTAuthMiddleware(AuthMiddlewareStack(inner))
