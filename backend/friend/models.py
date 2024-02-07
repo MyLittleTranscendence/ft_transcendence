ab03995786067bcb20582594044a6615cfe05779
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-07 16:38:48 +0900

    feat(friend): friend 모델 추가

--- backend/friend/models.py
+++ backend/friend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Friend(models.Model):
+    relate_user = models.ForeignKey(User, related_name="relate_users", on_delete=models.CASCADE)
+    friend_user = models.ForeignKey(User, related_name="friend_users", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['relate_user', 'friend_user'], name='unique_friend')
+        ]
