f06c1b32056b727aeefc5548a11d1a0be62b2de1
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-07 14:07:11 +0900

    feat(block): blocker, blocking 유니크 제약조건 추가

--- backend/block/models.py
+++ backend/block/models.py
@@ -6,3 +6,8 @@ from user.models import User
 class BlockUser(models.Model):
     blocker = models.ForeignKey(User, related_name="blockers", on_delete=models.CASCADE)
     blocking = models.ForeignKey(User, related_name="blockings", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['blocker', 'blocking'], name='unique_blocking')
+        ]
