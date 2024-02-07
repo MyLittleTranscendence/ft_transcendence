5f47d042fb197001558db1f8949757625ec344cb
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-07 20:16:09 +0900

    feat(friend): 로그인 시 친구 접속 정보 반환

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,18 +1,26 @@
 import json
 
+import aioredis
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
 from django.db.models import Q
 from django.utils.timezone import now
 
+from backend import settings
 from block.models import BlockUser
+from friend.models import Friend
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     LOGIN_GROUP = "login_group"
     TOTAL_MESSAGE = "total_message"
     SINGLE_MESSAGE = "single_message"
+    REDIS_HOST, REDIS_PORT = settings.CHANNEL_LAYERS["default"]["CONFIG"]["hosts"][0]
+
+    async def redis_connection(self):
+        self.redis = await aioredis.from_url(f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}", encoding="utf-8",
+                                             decode_responses=True)
 
     async def connect(self):
         if isinstance(self.scope['user'], AnonymousUser):
@@ -21,11 +29,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.channel_layer.group_add(self.LOGIN_GROUP, self.channel_name)
             await self.channel_layer.group_add(str(self.scope['user'].id), self.channel_name)
             await self.accept()
+            await self.redis_connection()
+            await self.redis.set(f"user:{str(self.scope['user'].id)}:online", 1)
+            await self.send_friend_status()
 
     async def disconnect(self, close_code):
-        if hasattr(self, self.LOGIN_GROUP):
-            await self.channel_layer.group_discard(self.LOGIN_GROUP, self.channel_name)
-            await self.channel_layer.group_discard(str(self.scope['user'].id), self.channel_name)
+        await self.redis.delete(f"user:{str(self.scope['user'].id)}:online")
+        await self.channel_layer.group_discard(self.LOGIN_GROUP, self.channel_name)
+        await self.channel_layer.group_discard(str(self.scope['user'].id), self.channel_name)
+        self.redis.close()
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -88,3 +100,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
             Q(blocker_id=sender_id, blocking_id=receiver_id) |
             Q(blocker_id=receiver_id, blocking_id=sender_id)
         ).exists()
+
+    # 친구 상태 반환
+    async def send_friend_status(self):
+        user_id = self.scope['user'].id
+        friends_status = await self.get_friends_status(user_id)
+        await self.send(text_data=json.dumps({
+            "type": "friend_status",
+            "friends_status": friends_status
+        }))
+
+    async def get_friends_status(self, user_id):
+        friends = await database_sync_to_async(list)(Friend.objects.filter(relate_user_id=user_id)
+                                                     .values_list('friend_user_id', flat=True))
+        friends_status = {}
+        for friend_id in friends:
+            is_online = await self.redis.exists(f"user:{str(friend_id)}:online")
+            friends_status[str(friend_id)] = is_online
+        return friends_status
