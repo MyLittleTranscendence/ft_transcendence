e886d37d62f79a59ac28135a80e8fce708cce2fc
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-07 17:02:50 +0900

    feat(friend): 친구목록 모델 메서드 구현

--- backend/friend/models.py
+++ backend/friend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from user.models import User
 
@@ -11,3 +12,13 @@ class Friend(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['relate_user', 'friend_user'], name='unique_friend')
         ]
+
+    @classmethod
+    def add_friend(cls, relate_user, friend_user):
+        if relate_user == friend_user:
+            raise ValidationError(detail={"detail": "자기 자신을 친구로 추가할 수 없습니다!"})
+        if cls.objects.filter(relate_user=relate_user, friend_user=friend_user).exists():
+            raise ValidationError(detail={"detail": "이미 친구로 추가한 유저입니다!"})
+        return cls.objects.create(relate_user=relate_user, friend_user=friend_user)
+
+
