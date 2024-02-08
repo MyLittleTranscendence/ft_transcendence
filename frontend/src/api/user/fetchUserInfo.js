505b39fb268e8703de3c47f75892bd1dbaad06fd
Clearsu <clearwater1219@gmail.com>
2024-02-08 16:05:04 +0900

    refactor(fetchUserInfo): return promise

--- frontend/src/api/user/fetchUserInfo.js
+++ frontend/src/api/user/fetchUserInfo.js
@@ -1,17 +1,19 @@
 import fetchAPI from "../../utils/fetchAPI.js";
 
-const fetchUserInfo = (userId) => {
+const fetchUserInfo = (userId) =>
   fetchAPI
     .get(`/users/${userId}/`)
     .then((data) => {
+      sessionStorage.setItem("user_id", data.id);
       sessionStorage.setItem("username", data.username);
       sessionStorage.setItem("nickname", data.nickname);
       sessionStorage.setItem("email", data.email);
       sessionStorage.setItem("wins", data.wins);
       sessionStorage.setItem("losses", data.losses);
       sessionStorage.setItem("profile_image", data.profile_image);
+      sessionStorage.setItem("mfa_require", data.mfa_enable);
+      return data;
     })
     .catch((error) => console.error(error));
-};
 
 export default fetchUserInfo;
