432cea9ec601be5b1346df8c1435bfc29c1cfb71
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-10 02:44:58 +0900

    feat(game): 기본 game consumer 정의

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,19 +1,16 @@
 import json
 
-import aioredis
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
 
-from backend import settings
+from backend.redis import RedisConnection
 from game.message_type import GameMessageType
+from game.service import GameService
 
 
 class GameConsumer(AsyncWebsocketConsumer):
-    REDIS_HOST, REDIS_PORT = settings.CHANNEL_LAYERS["default"]["CONFIG"]["hosts"][0]
-
-    async def redis_connection(self):
-        self.redis = await aioredis.from_url(f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}", encoding="utf-8",
-                                             decode_responses=True)
+    redis = None
+    game_service = None
 
     async def connect(self):
         if isinstance(self.scope['user'], AnonymousUser):
@@ -22,14 +19,25 @@ class GameConsumer(AsyncWebsocketConsumer):
             await self.channel_layer.group_add(GameMessageType.LOGIN_GROUP, self.channel_name)
             await self.channel_layer.group_add(str(self.scope['user'].id), self.channel_name)
             await self.accept()
-            await self.redis_connection()
+            redis_connection = await RedisConnection.get_instance()
+            self.redis = redis_connection.redis
+            self.game_service = await GameService.get_instance()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(GameMessageType.LOGIN_GROUP, self.channel_name)
         await self.channel_layer.group_discard(str(self.scope['user'].id), self.channel_name)
-        self.redis.close()
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message_type = text_data_json.get('type')
+        if message_type == GameMessageType.SINGLE_GAME_CREATE:
+            await self.game_service.start_single_pingpong_game(self.scope['user'].id)
 
+    async def single_game(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "game_update",
+            "bar_x": event["bar_x"],
+            "bar_y": event["bar_y"],
+            "circle_x": event["circle_x"],
+            "circle_y": event["circle_y"],
+        }))
