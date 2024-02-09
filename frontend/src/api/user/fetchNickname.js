8e377ed5ed57e6801dcc2e672543051d70417d91
Clearsu <clearwater1219@gmail.com>
2024-02-09 20:03:04 +0900

    feat: fetchNickname

---
+++ frontend/src/api/user/fetchNickname.js
@@ -0,0 +1,27 @@
+import fetchAPI from "../../utils/fetchAPI.js";
+import getRouter from "../../core/router.js";
+import showToast from "../../utils/showToast.js";
+
+const fetchNickname = (body) => {
+  const searchParams = new URLSearchParams(window.location.search);
+  const userId =
+    searchParams.get("user_id") || sessionStorage.getItem("user_id");
+
+  if (!userId) {
+    showToast("No user ID found.");
+    return;
+  }
+
+  fetchAPI
+    .patch(`/users/${userId}/`, body)
+    .then(() => {
+      if (window.location.pathname === "/profile") {
+        showToast("Your nickname has set successfully!");
+      } else {
+        getRouter().navigate("/?login=true"); // initial OAuth user
+      }
+    })
+    .catch((e) => showToast(e));
+};
+
+export default fetchNickname;
