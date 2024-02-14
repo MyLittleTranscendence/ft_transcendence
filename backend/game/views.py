867d115c3701985b40b29d419a89b6f2aafe1716
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-15 01:04:39 +0900

    feat(game): 게임 뷰 추가

--- backend/game/views.py
+++ backend/game/views.py
@@ -1,4 +1,11 @@
+from django.db.models import Q
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from game.models import Game
+from game.serializers import GameListSerializer
+from user.models import User
 
 
 def single_game(request):
@@ -11,3 +18,17 @@ def multi_game(request):
 
 def tournament_game(request):
     return render(request, "game/tournament_game.html")
+
+
+class GameListView(ListAPIView):
+    serializer_class = GameListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user_id', None)
+        if user_id is not None:
+            user = User.objects.get(pk=user_id)
+            return Game.objects.filter(Q(left_user=user) | Q(right_user=user)).order_by("-id")
+        else:
+            return Game.objects.all().order_by("id").order_by("-id")
+
