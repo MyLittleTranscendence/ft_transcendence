036295476ee6925972af24aaaecb6f6950be55eb
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2024-02-15 21:22:43 +0900

    feat: 매칭 섹션 상세 UI 구현 (#200)
    * feat: cancle button
    
    * feat(UI): match box

--- frontend/src/components/Lobby/MatchTypeDropdown.js
+++ frontend/src/components/Lobby/MatchTypeDropdown.js
@@ -1,22 +1,39 @@
 import Component from "../../core/Component.js";
+import appendCSSLink from "../../utils/appendCSSLink.js";
 import Button from "../UI/Button/Button.js";
+
+appendCSSLink("src/components/Lobby/MatchTypeDropdown.css");
 
 export default class MatchTypeDropdown extends Component {
   template() {
     return `
-      <ul class="dropdown-menu border rounded-5">
-        <li class="text-center fs-5 fw-bold">1 vs 1</li>
-        <li class="text-center fs-5 fw-bold">Tournament</li>
+      <div
+        id="match-type-btn-holder"
+        ${this.props.isFindingMatch ? "" : `data-bs-toggle="dropdown"`}
+        aria-expanded="false"
+      ></div>
+      <ul 
+        class="dropdown-menu rounded-5 match-type-dropdown-ul fs-5 fw-bold text-center"
+      >
+        <li class="match-type-dropdown-li">1 vs 1</li>
+        <li class="match-type-dropdown-li">Tournament</li>
+        <li class="match-type-dropdown-li">Single Play</li>
       </ul>
     `;
   }
 
   mounted() {
-    const matchTypeButton = new Button(this.$target, {
-      content: "Choose Match Type",
-      className: "dropdown-toggle",
-      attributes: `data-bs-toggle="dropdown" aria-expanded="false"`,
-    });
+    const { matchType, isFindingMatch } = this.props;
+
+    const matchTypeButton = new Button(
+      this.$target.querySelector("#match-type-btn-holder"),
+      {
+        content: matchType,
+        className: "dropdown-toggle",
+        attributes: `style="width: 15rem"`,
+        disabled: isFindingMatch,
+      }
+    );
     matchTypeButton.render();
   }
 }
