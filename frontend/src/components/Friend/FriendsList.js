53ec4aa7ee69d71fb68e41060879edb99dc54b94
ddang-jung <hjk3376@gmail.com>
2024-02-15 19:51:48 +0900

    feat: friends page init

--- frontend/src/components/Friend/FriendsList.js
+++ frontend/src/components/Friend/FriendsList.js
@@ -102,3 +102,35 @@ export default class FriendsList extends Component {
     });
   }
 }
+import Component from "../../core/Component.js";
+
+export default class FriendsList extends Component {
+  template() {
+    const { friends } = this.props;
+    return `
+      <div
+        class="list-group list-group-flush"
+      >
+        <div id="friends-list-holder">
+        ${friends.map(
+          (friend) => `
+            <div
+              class="list-group-item"
+            >
+              <div>
+                <img
+                  src=${friend.profile_image}
+                  class="img-fluid rounded-circle"
+                  alt="default"
+                >
+              </div>
+              <span>
+                <button class="btn">${friend.nickname}</button>
+              </span>
+            </div>`
+        )}
+        </div>
+      </div>
+    `;
+  }
+}
