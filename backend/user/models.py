da9c237ecb72ddb7e336fb2d461c73579e720015
SeungChul Chung <wjdtmdcjf2@gmail.com>
2024-02-17 02:29:12 +0900

    refactor(user): 리팩토링 및 문서화 (#221)
    * docs(game): users view
    * docs(game): users models
    * refactor(user): models error 메시지
    * refactor(user): models error 메시지

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,17 +1,20 @@
 import uuid
 from datetime import datetime, timedelta
 
-import pytz
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 from rest_framework.exceptions import PermissionDenied, AuthenticationFailed
 
 from backend import settings
+from backend.error_messages import Error
 
 
 class UserManager(BaseUserManager):
     def create_user(self, username, email, nickname, password=None):
+        """
+        사용자 회원가입
+        """
         user = self.model(
             username=username,
             email=self.normalize_email(email),
@@ -26,6 +29,9 @@ class UserManager(BaseUserManager):
         return user
 
     def create_oauth_user(self, email):
+        """
+        oauth 유저 회원가입
+        """
         username = f'user_{uuid.uuid4()}'
         email = self.normalize_email(email)
         nickname = f'pingpong_{uuid.uuid4()}'
@@ -34,6 +40,9 @@ class UserManager(BaseUserManager):
 
 
 def upload_to(instance, filename):
+    """
+    이미지 저장 경로 결정
+    """
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
     return f'profile_images/{filename}'
@@ -51,6 +60,9 @@ class User(AbstractUser):
     objects = UserManager()
 
     def update_mfa_code(self):
+        """
+        mfa code 발급 정보 저장
+        """
         code = str(uuid.uuid4())
         self.mfa_code = code
         self.mfa_generate_time = datetime.now()
@@ -58,35 +70,50 @@ class User(AbstractUser):
         return self
 
     def mfa_code_check(self, mfa_code):
+        """
+        mfa 코드 검증
+        """
         utc_now = datetime.now()
         if not self.mfa_enable:
-            raise PermissionDenied('bad access, efa disabled')
+            raise PermissionDenied(Error.EFA_DISABLED)
         if utc_now - timedelta(minutes=settings.MFA_LIMIT_TIME) > self.mfa_generate_time:
-            raise AuthenticationFailed("Code Timeout")
+            raise AuthenticationFailed(Error.CODE_TIMEOUT)
         if self.mfa_code != mfa_code:
-            raise AuthenticationFailed("Code Invalid")
+            raise AuthenticationFailed(Error.CODE_INVALID)
 
     def update_mfa_disable(self):
+        """
+        mfa disable
+        """
         if not self.mfa_enable:
-            raise PermissionDenied('bad access, 2fa already disabled')
+            raise PermissionDenied(Error.EFA_ALREADY_DISABLED)
         self.mfa_enable = False
         self.save(update_fields=['mfa_enable'])
 
     def update_mfa_enable(self, mfa_code):
+        """
+        mfa enable
+        """
         utc_now = datetime.now()
         if self.mfa_enable:
-            raise PermissionDenied('bad access, 2fa already enabled')
+            raise PermissionDenied(Error.EFA_ALREADY_ENABLED)
         if utc_now - timedelta(minutes=settings.MFA_LIMIT_TIME) > self.mfa_generate_time:
-            raise AuthenticationFailed("Code Timeout")
+            raise AuthenticationFailed(Error.CODE_TIMEOUT)
         if self.mfa_code != mfa_code:
-            raise AuthenticationFailed("Code Invalid")
+            raise AuthenticationFailed(Error.CODE_INVALID)
         self.mfa_enable = True
         self.save(update_fields=['mfa_enable'])
 
     def increase_wins(self):
+        """
+        승리 카운트 증가
+        """
         self.wins += 1
         self.save()
 
     def increase_losses(self):
+        """
+        패배 카운트 증가
+        """
         self.losses += 1
         self.save()
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,12 +1,14 @@
+from django.shortcuts import get_object_or_404
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import permissions, viewsets, status
+from rest_framework import permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from backend.checkers import serializer_valid_check
 from user.serializers import UserGetSerializer, UserPatchSerializer, UserPostSerializer, UserProfileImageSerializer, \
     UserCheckSerializer, UserMyProfileSerializer
 from .models import User
@@ -14,10 +16,16 @@ from .models import User
 
 class IsOwnerOrAdminUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        """
+        리소스 주인, 관리자만 사용 가능한 권한 제어
+        """
         return request.user and (request.user.is_staff or obj.id == request.user.id)
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    user 관련 기본 crud api
+    """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserGetSerializer
 
@@ -44,12 +52,18 @@ class MyProfileGetView(APIView):
 
     @swagger_auto_schema(responses={200: UserMyProfileSerializer})
     def get(self, request):
+        """
+        자신의 프로필 조회
+        """
         user = request.user
         serializer = UserMyProfileSerializer(user, context={'request': request})
         return Response(serializer.data)
 
 
 class UserProfileUpdateView(APIView):
+    """
+    profile_image 변경
+    """
     parser_classes = [MultiPartParser]
     permission_classes = [IsOwnerOrAdminUser]
 
@@ -66,10 +80,9 @@ class UserProfileUpdateView(APIView):
         responses={200: UserProfileImageSerializer}
     )
     def put(self, request, pk):
-        user = User.objects.get(pk=pk)
+        user = get_object_or_404(User, pk=pk)
         serializer = UserProfileImageSerializer(user, data=request.data, context={'request': request})
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer_valid_check(serializer)
         serializer.save()
         return Response(serializer.data)
 
@@ -84,6 +97,9 @@ class UserCheckViewSet(viewsets.GenericViewSet):
     )
     @action(methods=['get'], detail=False, url_path='nickname', pagination_class=None)
     def nickname_check(self, request):
+        """
+        nickname 중복 체크
+        """
         nickname = request.query_params.get('nickname')
         exists = User.objects.filter(nickname=nickname).exists()
         serializer = UserCheckSerializer(exists)
@@ -96,6 +112,9 @@ class UserCheckViewSet(viewsets.GenericViewSet):
     )
     @action(methods=['get'], detail=False, url_path='username', pagination_class=None)
     def username_check(self, request):
+        """
+        username 중복 체크
+        """
         username = request.query_params.get('username')
         exists = User.objects.filter(username=username).exists()
         serializer = UserCheckSerializer(exists)
