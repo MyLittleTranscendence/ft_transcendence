a7d8cfb23a4382961519a87f1e5c17d7d86bc389
middlefitting <wjdtmdcjf2@gmail.com>
2024-02-17 05:49:22 +0900

    fix(chat): 문서화 및 그룹 중복 버그 해

--- backend/backend/consumers.py
+++ backend/backend/consumers.py
@@ -2,7 +2,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
 
 from backend.redis import RedisConnection
-from game.message_type import GameMessageType
 
 
 class DefaultConsumer(AsyncWebsocketConsumer):
@@ -12,12 +11,12 @@ class DefaultConsumer(AsyncWebsocketConsumer):
         if isinstance(self.scope['user'], AnonymousUser):
             await self.close(code=4001)
         else:
-            await self.channel_layer.group_add(GameMessageType.LOGIN_GROUP, self.channel_name)
-            await self.channel_layer.group_add(str(self.scope['user'].id), self.channel_name)
+            await self.channel_layer.group_add(self.LOGIN_GROUP, self.channel_name)
             await self.accept()
             redis_connection = await RedisConnection.get_instance()
             self.redis = redis_connection.redis
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(GameMessageType.LOGIN_GROUP, self.channel_name)
-        await self.channel_layer.group_discard(str(self.scope['user'].id), self.channel_name)
+        if not isinstance(self.scope['user'], AnonymousUser):
+            await self.channel_layer.group_discard(self.LOGIN_GROUP, self.channel_name)
+            await self.channel_layer.group_discard(str(self.scope['user'].id), self.channel_name)
