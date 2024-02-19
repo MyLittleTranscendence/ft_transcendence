c508be6f679e6f351dc6eb87f4e77b2eca4b57d5
Clearsu <clearwater1219@gmail.com>
2024-02-19 21:21:43 +0900

    refactor: change prop name

--- frontend/src/components/Game/NextMatchBox.js
+++ frontend/src/components/Game/NextMatchBox.js
@@ -3,7 +3,7 @@ import ProfileImage from "../UI/Profile/ProfileImage.js";
 
 export default class NextMatchBox extends Component {
   template() {
-    const { player1, player2 } = this.props;
+    const { leftUser, rightUser } = this.props;
 
     return `
       <div
@@ -13,9 +13,9 @@ export default class NextMatchBox extends Component {
         <h6 class="text-white">Next Match</h6>
         <div class="d-flex align-items-center text-warning fw-bold">
           <div id="next-player-1-holder"></div>
-          <div class="mx-2 text-white">${player1.nickname}</div>
+          <div class="mx-2 text-white">${leftUser.nickname}</div>
           vs
-          <div class="mx-2 text-white">${player2.nickname}</div>
+          <div class="mx-2 text-white">${rightUser.nickname}</div>
           <div id="next-player-2-holder"></div>
         </div>
       </div>
@@ -23,21 +23,21 @@ export default class NextMatchBox extends Component {
   }
 
   mounted() {
-    const player1Image = new ProfileImage(
+    const leftUserImage = new ProfileImage(
       this.$target.querySelector("#next-player-1-holder"),
       {
         imageSrc: "asset/default.png",
         imageSize: "image-xs",
       }
     );
-    const player2Image = new ProfileImage(
+    const rightUserImage = new ProfileImage(
       this.$target.querySelector("#next-player-2-holder"),
       {
         imageSrc: "asset/default.png",
         imageSize: "image-xs",
       }
     );
-    player1Image.render();
-    player2Image.render();
+    leftUserImage.render();
+    rightUserImage.render();
   }
 }
