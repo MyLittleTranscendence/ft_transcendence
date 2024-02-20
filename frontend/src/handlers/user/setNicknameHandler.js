632c15d9cc84785c5f2a7463310b5b4e3ccc4ee6
Clearsu <clearwater1219@gmail.com>
2024-02-20 20:37:56 +0900

    feat: nickname minimum length

--- frontend/src/handlers/user/setNicknameHandler.js
+++ frontend/src/handlers/user/setNicknameHandler.js
@@ -1,12 +1,17 @@
 import fetchNickname from "../../api/user/fetchNickname.js";
+import showToast from "../../utils/showToast.js";
 
 const handleSubmitNickname = (event) => {
   event.preventDefault();
-  const body = {
-    nickname: event.srcElement.querySelector("#nickname-input").value,
-  };
 
-  fetchNickname(body);
+  const nickname = event.srcElement.querySelector("#nickname-input").value;
+
+  if (nickname.length <= 3) {
+    showToast("Nickname should be at least 4 characters.");
+    return;
+  }
+
+  fetchNickname({ nickname });
 };
 
 export default handleSubmitNickname;
