fcca335aedf0de17864f71ba016489aa3b47c9a0
SeungChul Chung <wjdtmdcjf2@gmail.com>
2024-02-21 15:22:46 +0900

    feat(exception_handler): 401 발생시 쿠키 즉시 만료 (#246)

--- backend/backend/exception_handler.py
+++ backend/backend/exception_handler.py
@@ -6,10 +6,13 @@ from django.db import IntegrityError
 
 def exception_handler(exc, context):
     response = drf_exception_handler(exc, context)
-
+    if response.status_code == 401:
+        response.set_cookie('access_token', value='', max_age=0)
+        return response
     if response is None:
         if isinstance(exc, IntegrityError):
             return Response({'detail': '중복된 데이터입니다'}, status=status.HTTP_409_CONFLICT)
+
         # else:
         #     return Response({'detail': '알 수 없는 에러입니다'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
