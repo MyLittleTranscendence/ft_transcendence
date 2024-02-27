96a0da5ebd6f1c96c287c3b82f08d9e3b87b3ce1
ddang-jung <hjk3376@gmail.com>
2024-02-27 15:29:59 +0900

    fix: password length 12 to 20

--- frontend/src/components/SignUp/SignUpForm.js
+++ frontend/src/components/SignUp/SignUpForm.js
@@ -71,9 +71,9 @@ export default class SignUpForm extends Component {
     const pwInputProps = {
       type: "password",
       id: "signup-form-pw",
-      pattern: "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{8,20}",
+      pattern: "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{12,20}",
       name: "signup-form",
-      placeholder: "8 ~ characters",
+      placeholder: "12 ~ characters",
       autocomplete: true,
       required: true,
     };
