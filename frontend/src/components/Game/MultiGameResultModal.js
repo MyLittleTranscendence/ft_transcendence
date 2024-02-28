8501f949ce8e150f21155ba3437d12f48704f318
ddang-jung <hjk3376@gmail.com>
2024-02-28 23:41:03 +0900

    style: lose to lost

--- frontend/src/components/Game/MultiGameResultModal.js
+++ frontend/src/components/Game/MultiGameResultModal.js
@@ -5,7 +5,7 @@ export default class MultiGameResultModal extends Modal {
   mounted() {
     this.$modalTitle.textContent = this.props.isWin
       ? "Congrats, You Won!"
-      : "Sorry, You Lose.";
+      : "Sorry, You Lost.";
     const goHomeLink = new Link(this.$modalButtonGroup, {
       id: "modal-close",
       href: "/",
