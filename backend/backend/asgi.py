7990faeb38ee33f8439f5ae0ee5030a04891c62c
SeungChul Chung <wjdtmdcjf2@gmail.com>
2024-03-04 23:06:43 +0900

    chore(backend): ssl 적용 (#331)

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,15 +1,16 @@
 # mysite/asgi.py
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
-from backend.middleware import JWTAuthMiddlewareStack
-from backend.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from backend.middleware import JWTAuthMiddlewareStack
+from backend.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
