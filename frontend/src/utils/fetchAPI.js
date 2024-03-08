58256be7e3165fd7218158694df436de757977f2
Clearsu <clearwater1219@gmail.com>
2024-03-08 18:45:10 +0900

    feat: https on frontend

--- frontend/src/utils/fetchAPI.js
+++ frontend/src/utils/fetchAPI.js
@@ -1,5 +1,5 @@
 const fetchRequest = async (url, requestOptions) => {
-  const response = await fetch(`http://localhost:3000/api${url}`, {
+  const response = await fetch(`https://localhost/api${url}`, {
     ...requestOptions,
     credentials: "include",
   });
